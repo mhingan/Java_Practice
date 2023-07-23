7d50a45518e7dca36dfc4ac8f6a7c59721991e3d
Mihaita Hingan <mhingan6@gmail.com>
2023-07-23 16:41:35 +0300

    Book Library - app - v 2.1

--- Java_Practice/src/main/java/org/example/Books_Library/Book.java
+++ Java_Practice/src/main/java/org/example/Books_Library/Book.java
@@ -8,14 +8,18 @@ public class Book {
     private int numberOfPages;
     private TYPE type;
     private boolean forAdultsOnly;
+    private int id;
+    private int quantity;
 
 
-    public Book(String name, String author, int numberOfPages, TYPE type, boolean forAdultsOnly) {
+    public Book(String name, String author, int numberOfPages, TYPE type, boolean forAdultsOnly, int id, int quantity) {
         this.name = name;
         this.author = author;
         this.numberOfPages = numberOfPages;
         this.type = type;
         this.forAdultsOnly = forAdultsOnly;
+        this.id = id;
+        this.quantity = quantity;
     }
 
     public String getName() {
@@ -41,6 +45,17 @@ public class Book {
     public void setAuthor(String author) {
         this.author = author;
     }
+    public int getId() {
+        return id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
     @Override
     public String toString() {
@@ -50,6 +65,8 @@ public class Book {
                 ", numberOfPages=" + numberOfPages +
                 ", type=" + type +
                 ", forAdultsOnly=" + forAdultsOnly +
+                ", id=" + id +
+                ", quantity=" + quantity +
                 '}';
     }
 
@@ -65,4 +82,8 @@ public class Book {
     public int hashCode() {
         return Objects.hash(name, author, numberOfPages, type, forAdultsOnly);
     }
+
+
+
+
 }
