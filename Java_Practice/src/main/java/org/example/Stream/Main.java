c57a0e004d09f529ddd0e4bf7170154925654e01
Mihaita Hingan <mhingan6@gmail.com>
2023-07-26 15:18:33 +0300

    stream ex 2

--- Java_Practice/src/main/java/org/example/Stream/Main.java
+++ Java_Practice/src/main/java/org/example/Stream/Main.java
@@ -5,9 +5,13 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        //exercise 1
         SHandler sHandler = new SHandler();
         List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,0);
 
-        sHandler.returnEvenNumbersFromList(numbers);
+        sHandler.exercise1(numbers);
+
+        //exercise2
+        sHandler.exercise2();
     }
 }
