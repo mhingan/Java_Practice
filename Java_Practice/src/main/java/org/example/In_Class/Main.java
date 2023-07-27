d46b6bbbac6f9f3424a192a951c5accab15cc766
Mihaita Hingan <mhingan6@gmail.com>
2023-07-27 16:37:20 +0300

    In class examples

---
+++ Java_Practice/src/main/java/org/example/In_Class/Main.java
@@ -0,0 +1,9 @@
+package org.example.In_Class;
+
+public class Main {
+    public static void main(String[] args) {
+            MyRunnable myRunnable = new MyRunnable();
+            myRunnable.run();
+
+    }
+}
---
+++ Java_Practice/src/main/java/org/example/In_Class/MyThread.java
@@ -0,0 +1,12 @@
+package org.example.In_Class;
+
+public class MyThread extends Thread{
+    @Override
+    public void run(){
+        System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getName());
+    }
+}
